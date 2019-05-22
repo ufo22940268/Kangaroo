c061acbbd50641b15cb8bc58db38132c805847d3
Frank Cheng <ufo22940268@gmail.com>
2019-05-22 20:45:28 +0800

    Initial Commit

---
+++ Kangaroo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kangaroo
+//
+//  Created by Frank Cheng on 2019/5/22.
+//  Copyright © 2019 Frank Cheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Kangaroo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kangaroo
+//
+//  Created by Frank Cheng on 2019/5/22.
+//  Copyright © 2019 Frank Cheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
