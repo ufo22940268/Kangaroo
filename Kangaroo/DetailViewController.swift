3f8b15684f186e421a6d1f276009dfb5956b9c95
Frank Cheng <ufo22940268@gmail.com>
2019-05-23 17:27:05 +0800

    v

---
+++ Kangaroo/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Kangaroo
+//
+//  Created by Frank Cheng on 2019/5/23.
+//  Copyright © 2019 Frank Cheng. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
